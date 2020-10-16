6f1285a7e73ae54065f6c683cdf07b7bc982d5e6
David Atanda <atandadave@gmail.com>
2020-10-16 16:28:02 +0100

    that is all

---
+++ index.js
@@ -0,0 +1,23 @@
+
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
+
+    var docWidth = document.documentElement.offsetWidth;
+
+    [].forEach.call(
+      document.querySelectorAll('*'),
+      function(el) {
+        if (el.offsetWidth > docWidth) {
+          console.log(el);
+        }
+      }
+    );
+
+
+
